eb8c9a0629b26f3ab707649477556b5864979a2f
qayyuum1984 <102579530+qayyuum1984@users.noreply.github.com>
2025-03-12 06:31:32 +0800

    Add app/src/main/java/com/example/mymutabaah/ui/theme/Color.kt 1741732291029

---
+++ app/src/main/java/com/example/mymutabaah/ui/theme/Color.kt
@@ -0,0 +1,50 @@
+package com.example.mymutabaah.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val PrimaryLight = Color(0xFF1F4B8E) // Deep Islamic Blue
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD5E3FF)
+val OnPrimaryContainerLight = Color(0xFF001B3D)
+
+val SecondaryLight = Color(0xFF705D00) // Warm Gold
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFFFE178)
+val OnSecondaryContainerLight = Color(0xFF221B00)
+
+val TertiaryLight = Color(0xFF006C4C) // Emerald Green
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFF89F8C7)
+val OnTertiaryContainerLight = Color(0xFF002115)
+
+val BackgroundLight = Color(0xFFFDFBFF)
+val OnBackgroundLight = Color(0xFF001B3D)
+val SurfaceLight = Color(0xFFFDFBFF)
+val OnSurfaceLight = Color(0xFF001B3D)
+
+// Dark Theme Colors
+val PrimaryDark = Color(0xFFA8C8FF)
+val OnPrimaryDark = Color(0xFF003063)
+val PrimaryContainerDark = Color(0xFF004890)
+val OnPrimaryContainerDark = Color(0xFFD5E3FF)
+
+val SecondaryDark = Color(0xFFE7C447)
+val OnSecondaryDark = Color(0xFF3A2F00)
+val SecondaryContainerDark = Color(0xFF544500)
+val OnSecondaryContainerDark = Color(0xFFFFE178)
+
+val TertiaryDark = Color(0xFF6CDBAC)
+val OnTertiaryDark = Color(0xFF003826)
+val TertiaryContainerDark = Color(0xFF005138)
+val OnTertiaryContainerDark = Color(0xFF89F8C7)
+
+val BackgroundDark = Color(0xFF001B3D)
+val OnBackgroundDark = Color(0xFFD5E3FF)
+val SurfaceDark = Color(0xFF001B3D)
+val OnSurfaceDark = Color(0xFFD5E3FF)
+
+// Additional Colors
+val CompletedGreen = Color(0xFF4CAF50)
+val PendingOrange = Color(0xFFFFA726)
+val ErrorRed = Color(0xFFB00020)
